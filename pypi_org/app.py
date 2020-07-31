31881a07c981a6bc4aa46836acbe8b326fbd7dc1
jp-sypniewski <jpsypniewski@icloud.com>
2020-07-31 12:22:44 -0600

    add site.css and reference in _layout.html, fill html structure for index.html

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -11,7 +11,9 @@ def main():
 def register_blueprints():
     print('in register_blueprints')
     from pypi_org.views import home_views
+    # from pypi_org.views import home_views
     app.register_blueprint(home_views.blueprint)
+    # app.register_blueprint(home_views.blueprint)
 
 
 if __name__ == '__main__':
--- pypi_org/services/package_service.py
+++ pypi_org/services/package_service.py
@@ -2,5 +2,5 @@ def get_latest_packages():
     return [
         {'name': 'flask', 'version': '1.2.3'},
         {'name': 'sqlalchemy', 'version': '2.2.0'},
-        {'name': 'passlib', 'version': '3.0.0'},
+        {'name': 'passlib', 'version': '3.0.0'}
     ]
