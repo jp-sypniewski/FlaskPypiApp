ccee7f5d3bdda20a3785e5ee7f271d9007435fef
jp-sypniewski <jpsypniewski@icloud.com>
2020-07-31 12:06:34 -0600

    updated project configuration to run as python app, home blueprint working

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -2,13 +2,14 @@ import flask
 
 app = flask.Flask(__name__)
 
-
 def main():
+    print('in main')
     register_blueprints()
     app.run(debug=False)
 
 
 def register_blueprints():
+    print('in register_blueprints')
     from pypi_org.views import home_views
     app.register_blueprint(home_views.blueprint)
 
--- pypi_org/views/home_views.py
+++ pypi_org/views/home_views.py
@@ -6,7 +6,7 @@ import pypi_org.services.package_service as package_service
 blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 
 @blueprint.route('/')
-@response(template_file='')
+@response(template_file='home/index.html')
 def index():
     test_packages = package_service.get_latest_packages()
     return {'packages': test_packages}
