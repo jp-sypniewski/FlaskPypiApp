31bcba7ad0d2d2a85ec04ee5477fcae57f65657e
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-01 13:42:13 -0600

    add db_session.py for sqlite, add setup_db in app.py

---
+++ pypi_org/data/db_session.py
@@ -0,0 +1,26 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+
+from pypi_org.data.modelbase import SqlAlchemyBase
+
+factory = None
+
+def global_init(db_file: str):
+    global factory
+
+    if factory:
+        return
+
+    if not db_file or not db_file.strip():
+        raise Exception("You must specify a db file.")
+
+    conn_str = 'sqlite:///' + db_file.strip()
+
+    # to show the queries run, set echo=True
+    engine = sa.create_engine(conn_str, echo=False)
+    factory = orm.sessionmaker(bind=engine)
+
+    # noinspection PyUnresolvedReferences
+    from pypi_org.data.package import Package
+
+    SqlAlchemyBase.metadata.create_all(engine)
