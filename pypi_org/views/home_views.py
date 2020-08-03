e1b51eea18ebe887e32f0a527a022d5e66521a8b
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-03 13:02:36 -0600

    stats showing correct numbers from db, create functions for package count and release count in package service, create user service with function for user count

--- pypi_org/views/home_views.py
+++ pypi_org/views/home_views.py
@@ -2,6 +2,7 @@ import flask
 
 from pypi_org.infrastructure.view_modifiers import response
 import pypi_org.services.package_service as package_service
+import pypi_org.services.user_service as user_service
 
 blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 
@@ -10,4 +11,9 @@ blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 def index():
     test_packages = package_service.get_latest_packages()
     print(str(flask.url_for('static', filename='css/site.css')))
-    return {'packages': test_packages}
+    return {
+        'packages': test_packages,
+        'package_count': package_service.get_package_count(),
+        'release_count': package_service.get_release_count(),
+        'user_count': user_service.get_user_count()
+    }
