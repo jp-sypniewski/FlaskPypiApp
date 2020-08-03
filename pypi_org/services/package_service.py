9f773847898eb17e5d3d53b62931c609e86955f6
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-03 13:43:21 -0600

    package page working with database query, eager loading of releases

--- pypi_org/services/package_service.py
+++ pypi_org/services/package_service.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Optional
+import sqlalchemy.orm as orm
 
 import pypi_org.data.db_session as db_session
 from pypi_org.data.package import Package
@@ -8,10 +9,13 @@ from pypi_org.data.releases import Release
 def get_latest_releases(limit = 10) -> List[Release]:
     session = db_session.create_session()
 
-    releases = session.query(Release).\
-        order_by(Release.created_date.desc()).\
-        limit(limit).\
-        all()
+    releases = session.query(Release)\
+        .options(orm.joinedload(Release.package))\
+        .order_by(Release.created_date.desc())\
+        .limit(limit)\
+        .all()
+
+    session.close()
 
     return releases
 
@@ -24,3 +28,18 @@ def get_release_count() -> int:
     return 200
     session = db_session.create_session()
     return session.query(Release).count()
+
+def get_package_by_id(package_id: str) -> Optional[Package]:
+    if not package_id:
+        return None
+
+    package_id = package_id.strip().lower()
+    session = db_session.create_session()
+    package = session.query(Package)\
+        .options(orm.joinedload(Package.releases))\
+        .filter(Package.id == package_id)\
+        .first()
+
+    session.close()
+
+    return package
