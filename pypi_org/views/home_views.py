10e6aee0edae6072c2353c63c5244b69c09043ec
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-03 13:15:39 -0600

    update package test method to release, accessing db for 10 latest packages

--- pypi_org/views/home_views.py
+++ pypi_org/views/home_views.py
@@ -9,10 +9,8 @@ blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 @blueprint.route('/')
 @response(template_file='home/index.html')
 def index():
-    test_packages = package_service.get_latest_packages()
-    print(str(flask.url_for('static', filename='css/site.css')))
     return {
-        'packages': test_packages,
+        'releases': package_service.get_latest_releases(),
         'package_count': package_service.get_package_count(),
         'release_count': package_service.get_release_count(),
         'user_count': user_service.get_user_count()
