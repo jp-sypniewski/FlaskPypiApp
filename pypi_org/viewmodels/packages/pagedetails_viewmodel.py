570a17763ce0008db546fc81a243c436657d9582
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-04 14:11:53 -0600

    create pagedetails viewmodel for packages views, refactor /project/<package_name> route to use viewmodel

---
+++ pypi_org/viewmodels/packages/pagedetails_viewmodel.py
@@ -0,0 +1,32 @@
+from pypi_org.services import package_service
+from pypi_org.viewmodels.shared.viewmodelbase import ViewModelBase
+
+
+class PackageDetailsViewModel(ViewModelBase):
+    def __init__(self, package_name: str):
+        super().__init__()
+
+        # create package_name and package fields for use immediately below
+        self.package_name = package_name
+        self.package = None
+
+        # if a package_name is passed, make that the field,
+        # then find the package from the database service
+        if package_name:
+            self.package_name = package_name.strip().lower()
+            self.package = package_service.get_package_by_id(self.package_name)
+
+        # create latest_version, latest_release, and is_latest
+        # fields for class
+        self.latest_version = "0.0.0"
+        self.latest_release = None
+        self.is_latest = True
+
+        # check if we found a package earlier and if there
+        # are releases attached to it
+        # if so, update the release related fields
+        if self.package and self.package.releases:
+            self.latest_release = self.package.releases[0]
+            self.latest_version = self.latest_release.version_text
+
+        self.release_version = self.latest_release
--- pypi_org/views/package_views.py
+++ pypi_org/views/package_views.py
@@ -1,32 +1,15 @@
 import flask
 
-import pypi_org.services.package_service as package_service
 from pypi_org.infrastructure.view_modifiers import response
+from pypi_org.viewmodels.packages.pagedetails_viewmodel import PackageDetailsViewModel
 
 blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 @blueprint.route('/project/<package_name>')
 @response(template_file='packages/details.html')
 def package_details(package_name: str):
-    if not package_name:
+    vm = PackageDetailsViewModel(package_name)
+    if not vm.package:
         return flask.abort(status=404)
 
-    package = package_service.get_package_by_id(package_name.strip().lower())
-    if not package:
-        return flask.abort(status=404)
-
-    latest_version = "0.0.0"
-    latest_release = None
-    is_latest = True
-
-    if package.releases:
-        latest_release = package.releases[0]
-        latest_version = latest_release.version_text
-
-    return {
-        'package': package,
-        'latest_version': latest_version,
-        'latest_release': latest_release,
-        'release_version': latest_release,
-        'is_latest': is_latest
-    }
+    return vm.to_dict()
