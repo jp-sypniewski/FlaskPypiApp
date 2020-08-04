20c7699b88c7725868bf5c5e170161077bac921c
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-04 15:01:17 -0600

    create sitemap viewmodel

--- pypi_org/services/package_service.py
+++ pypi_org/services/package_service.py
@@ -6,28 +6,29 @@ from pypi_org.data.package import Package
 from pypi_org.data.releases import Release
 
 
-def get_latest_releases(limit = 10) -> List[Release]:
+def get_latest_releases(limit=10) -> List[Release]:
     session = db_session.create_session()
 
-    releases = session.query(Release)\
-        .options(orm.joinedload(Release.package))\
-        .order_by(Release.created_date.desc())\
-        .limit(limit)\
+    releases = session.query(Release) \
+        .options(orm.joinedload(Release.package)) \
+        .order_by(Release.created_date.desc()) \
+        .limit(limit) \
         .all()
 
     session.close()
 
     return releases
 
+
 def get_package_count() -> int:
-    return 200
     session = db_session.create_session()
     return session.query(Package).count()
 
+
 def get_release_count() -> int:
-    return 200
     session = db_session.create_session()
     return session.query(Release).count()
+
 
 def get_package_by_id(package_id: str) -> Optional[Package]:
     if not package_id:
@@ -35,11 +36,18 @@ def get_package_by_id(package_id: str) -> Optional[Package]:
 
     package_id = package_id.strip().lower()
     session = db_session.create_session()
-    package = session.query(Package)\
-        .options(orm.joinedload(Package.releases))\
-        .filter(Package.id == package_id)\
+    package = session.query(Package) \
+        .options(orm.joinedload(Package.releases)) \
+        .filter(Package.id == package_id) \
         .first()
 
     session.close()
 
     return package
+
+
+def all_packages(limit=10) -> List[Package]:
+    session = db_session.create_session()
+    packages = session.query(Package).limit(limit).all()
+
+    return packages
