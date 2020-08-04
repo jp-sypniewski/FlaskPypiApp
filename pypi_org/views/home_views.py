4a2bf1afdff0dd09320bdc5f87daf09edc7b1347
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-04 13:58:45 -0600

    create index viewmodel for home, refactor home route to use viewmodel rather than directly reference service layer

--- pypi_org/views/home_views.py
+++ pypi_org/views/home_views.py
@@ -1,17 +1,13 @@
 import flask
 
 from pypi_org.infrastructure.view_modifiers import response
-import pypi_org.services.package_service as package_service
-import pypi_org.services.user_service as user_service
+from pypi_org.viewmodels.home.index_viewmodel import IndexViewModel
 
 blueprint = flask.Blueprint('home', __name__, template_folder='templates')
+
 
 @blueprint.route('/')
 @response(template_file='home/index.html')
 def index():
-    return {
-        'releases': package_service.get_latest_releases(),
-        'package_count': package_service.get_package_count(),
-        'release_count': package_service.get_release_count(),
-        'user_count': user_service.get_user_count()
-    }
+    vm = IndexViewModel()
+    return vm.to_dict()
