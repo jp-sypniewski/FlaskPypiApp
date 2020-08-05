095d162a88cbc20556f11868d2162c9e5ac82967
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-05 17:05:27 -0600

    refactor release information for single package view to reflect changes for mongo

--- pypi_org/viewmodels/packages/pagedetails_viewmodel.py
+++ pypi_org/viewmodels/packages/pagedetails_viewmodel.py
@@ -25,8 +25,9 @@ class PackageDetailsViewModel(ViewModelBase):
         # check if we found a package earlier and if there
         # are releases attached to it
         # if so, update the release related fields
-        if self.package and self.package.releases:
-            self.latest_release = self.package.releases[0]
-            self.latest_version = self.latest_release.version_text
+        if self.package:
+            self.latest_release = package_service.get_latest_release_for_package(self.package.id)
+            if self.latest_release:
+                self.latest_version = self.latest_release.version_text
 
         self.release_version = self.latest_release
